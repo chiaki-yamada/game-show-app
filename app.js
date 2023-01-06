4d4c1a5552da871cf5c15b1b5ec6e8a25e06ddd4
chiaki-yamada <110213744+chiaki-yamada@users.noreply.github.com>
2023-01-05 18:54:59 -0800

    Updated app.js

--- app.js
+++ app.js
@@ -1,2 +1,4 @@
 const qwerry = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
+const btnReset = document.getElementsByClassName('btn__reset');
+let missed = 0;
