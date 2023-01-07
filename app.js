4502e15033be81c3b26398a2ba8b4ef0e4372213
chiaki-yamada <110213744+chiaki-yamada@users.noreply.github.com>
2023-01-06 19:02:55 -0800

    Updated

--- app.js
+++ app.js
@@ -2,3 +2,10 @@ const qwerry = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const btnReset = document.getElementsByClassName('btn__reset');
 let missed = 0;
+
+let phrases = [ A, B, C, D, E ];
+
+const overlay =getElementById('overlay');
+btnReset.addEventListener ('click', () => {
+  overlay.style.display = 'hide';
+} );
