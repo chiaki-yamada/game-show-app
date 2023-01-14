12383685cc301feee6cdb1011d573c80fa99e5d7
chiaki-yamada <110213744+chiaki-yamada@users.noreply.github.com>
2023-01-14 12:16:23 -0800

    Update app.js

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
 const btnReset = document.querySelector('.btn__reset');
 const overlay = document.getElementById('overlay');
+const heart = document.querySelectorAll('img');
 let missed = 0;
 
 
@@ -57,3 +58,34 @@ const checkLetter = button => {
   } 
   return match;
 }
+
+qwerty.addEventListener('click', (e)=> {
+  if (e.target.tagName === 'BUTTON' && e.target.className !== 'chosen'){
+    e.target.classList.add('chosen');
+    const letterFound = checkLetter(e.target.textContent);
+    if (letterFound === null) {
+      heart[missed].src = 'img/lostHeart.png'; 
+      missed++;
+    }
+  }
+  checkWin();
+})
+
+const checkWin = () => {
+  const letterLi = document.getElementsByClassName('letter');
+  const showLi = document.getElementsByClassName('show');
+  const title = document.querySelector('.title');
+  if (letterLi.length === showLi.length){
+    overlay.classList.add('win');
+    title.innerHTML = "Congrats! You Won!";
+    overlay.style.display = 'flex';
+  } else if (missed > 4){
+    overlay.classList.add('lost');
+    title.innerHTML = "Oops! You Lost :(";
+    overlay.style.display = 'flex';
+  }
+}
+
+
+
+
